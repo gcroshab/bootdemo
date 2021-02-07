e95ad762cbc49d1a3f8bca6a1f41f5be9577bae7
roshan <c-roshan.gc@charter.com>
2021-02-06 20:53:04 -0600

    tesst  method

--- src/test/java/com/example/demo/BootDemoApplicationTests.java
+++ src/test/java/com/example/demo/BootDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,8 @@ public class BootDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+		Assert.assertEquals(true, true);
 	}
 
 }
